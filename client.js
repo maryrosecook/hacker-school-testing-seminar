ee0ee85f61f069e8c87a9188ba9086467e4328f1
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-07-01 22:12:40 -0400

    Split client into ajax get(), getTime() and displayTime()
    * Get request (IO) is completely abstracted into get().
    * Now have clean, reusable ajax get fn.
    * IO now separated from rest of program
    * Display code is separated.
    * Time data parsing is separated.
    * Note how fn names are way more descriptive.

--- client.js
+++ client.js
@@ -1,22 +1,27 @@
 ;(function(exports) {
-  exports.loadTime = function() {
+  exports.get = function(url, callback) {
     var req = new XMLHttpRequest();
-    req.onload = function(event) {
-      var ctx = document.getElementById("canvas").getContext('2d');
-      var responseJSON = JSON.parse(event.target.responseText);
-      var time = responseJSON.time;
-
-      var center = canvasRenderer.center(ctx);
-      if (time === "day") {
-        canvasRenderer.fillBackground(ctx, "#00f");
-        canvasRenderer.fillCircle(ctx, center.x, center.y, 30, "#ff0");
-      } else if (time === "night") {
-        canvasRenderer.fillBackground(ctx, "#000");
-        canvasRenderer.fillCircle(ctx, center.x, center.y, 30, "#aaa");
-      }
-    };
-
-    req.open("GET", "/time.json", true);
+    req.onload = callback;
+    req.open("GET", url, true);
     req.send();
+  };
+
+  exports.getTime = function(callback) {
+    exports.get('/time.json', function(data) {
+      var responseJSON = JSON.parse(data.target.responseText);
+      callback(responseJSON.time);
+    });
+  };
+
+  exports.displayTime = function(time) {
+    var ctx = document.getElementById("canvas").getContext('2d');
+    var center = canvasRenderer.center(ctx);
+    if (time === "day") {
+      canvasRenderer.fillBackground(ctx, "#00f");
+      canvasRenderer.fillCircle(ctx, center.x, center.y, 30, "#ff0");
+    } else if (time === "night") {
+      canvasRenderer.fillBackground(ctx, "#000");
+      canvasRenderer.fillCircle(ctx, center.x, center.y, 30, "#aaa");
+    }
   };
 })(this);
